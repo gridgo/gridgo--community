20c4bf159f1906157185567a5c9564bd0b5b185e
Anh Dung Bui <dungba.sg@gmail.com>
2019-08-12 12:46:10 +0700

    log exception if thrown

--- connectors/gridgo-xrpc/src/main/java/io/gridgo/xrpc/impl/dynamic/CorrIdXrpcResponder.java
+++ connectors/gridgo-xrpc/src/main/java/io/gridgo/xrpc/impl/dynamic/CorrIdXrpcResponder.java
@@ -1,16 +1,17 @@
 package io.gridgo.xrpc.impl.dynamic;
 
-import static org.joo.promise4j.DeferredStatus.RESOLVED;
-
 import java.util.UUID;
 
 import org.joo.promise4j.Deferred;
+import org.joo.promise4j.Promise;
 import org.joo.promise4j.impl.CompletableDeferredObject;
 
 import io.gridgo.bean.BObject;
 import io.gridgo.framework.support.Message;
 import io.gridgo.framework.support.Payload;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class CorrIdXrpcResponder extends EndpointDirectXrpcResponder {
 
     @Override
@@ -25,20 +26,25 @@ public class CorrIdXrpcResponder extends EndpointDirectXrpcResponder {
 
         final var replyTo = headers.getString("replyTo", null);
 
-        deferred.promise().always((stt, responseBody, ex) -> {
-            Message response;
-            if (stt == RESOLVED) {
-                response = Message.ofAny(responseBody);
-            } else {
-                response = Message.ofAny(ex == null ? "internal server error" : ex.getMessage());
-                String traceId = UUID.randomUUID().toString();
-                response.getPayload().addHeader("traceId", traceId);
-                response.getPayload().addHeader("timestamp", System.currentTimeMillis());
-            }
-
-            response.getPayload().addHeader("corrId", corrId);
-            sendResponse(replyTo, response);
-        });
+        deferred.promise() //
+                .map(Message::ofAny) //
+                .pipeFail(this::buildErrorMessage) //
+                .then(msg -> {
+                    msg.getPayload().addHeader("corrId", corrId);
+                    sendResponse(replyTo, msg);
+                    return Promise.of(null);
+                })//
+                .fail(ex -> {
+                    log.error("Error caught while sending response", ex);
+                });
         return deferred;
+    }
+
+    protected Promise<Message, Throwable> buildErrorMessage(Throwable ex) {
+        Message response = Message.ofAny(ex == null ? "internal server error" : ex.getMessage());
+        String traceId = UUID.randomUUID().toString();
+        response.getPayload().addHeader("traceId", traceId);
+        response.getPayload().addHeader("timestamp", System.currentTimeMillis());
+        return Promise.of(response);
     }
 }
